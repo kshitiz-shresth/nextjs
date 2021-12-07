e49d80c2a1b588878b7ea27c4f665f761b7c3da0
kshitiz-shresth <er.kshitizshrestha@gmail.com>
2021-12-07 14:46:51 +0545

    some changes

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,40 @@ import Navbar from '../components/Navbar'
 import MyHead from '../components/MyHead'
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
+import Link from 'next/dist/client/link';
 
 import { Carousel, Container } from 'react-bootstrap'
 
 const index = () => {
   return (
-    <Container bg="fluid">
+    <>
       <Head>
         <title>InstaBand</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section>
-      
 
+      <section>
+        <Carousel fade>
+          <Carousel.Item className={`${styles.sliderBox}`}>
+            <img
+              className={`d-block w-100 ${styles.sliderBox__image}`}
+              src="/images/hero-img-1.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <Link href="/dashboard"><a className={`insta-btn d-none d-md-inline`}>Explore</a></Link>
+              <p className={`mt-3`}>With one click, you will see multiple bands available for your big day.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </section>
-    </Container>
+      <section className={``}>
+        <Container>
+             
+        </Container>
+      </section>
+
+    </>
   )
 }
 export default index
